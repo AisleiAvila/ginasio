d1dc11ee82154ad03489b959d6d17b6ec8900796
Aislei Ávila de Medeiros <51420423+AisleiAvila@users.noreply.github.com>
2023-11-20 20:30:33 -0300

    Add files via upload

---
+++ src/main/java/br/com/dasad/ginasio/GinasioApplication.java
@@ -0,0 +1,13 @@
+package br.com.dasad.ginasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinasioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dasad/ginasio/GinasioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dasad.ginasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
